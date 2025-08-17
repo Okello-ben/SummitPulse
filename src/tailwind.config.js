f2f28967933eeec05a3e11488aaf8582d2c6cf22
Benedict Okello <sikes.ben@hotmail.com>
2025-08-17 07:26:15 +0300

    SummitPulse

---
+++ src/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Components/**/*.[razor,css,js]","./wwwroot/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
